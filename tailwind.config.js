df69e4455b49713e5c65b22b309f2165f54673a8
Mateo Barisch Somavilla <mateo.somavilla@pucpr.edu.br>
2026-05-06 10:49:05 -0300

    ui fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,3 +7,20 @@ export default {
   },
   plugins: [],
 };
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
