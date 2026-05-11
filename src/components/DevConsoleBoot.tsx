e46bdeb4512787bf60536b8dd97e1353c0694aa0
Logoali231 <logoali231@gmail.com>
2026-05-11 12:53:53 -0300

    Create DevConsoleBoot.tsx

---
+++ src/components/DevConsoleBoot.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { useEffect } from "react";
+
+import {
+  initDevConsole,
+} from "@/lib/devConsole";
+
+export default function DevConsoleBoot() {
+  useEffect(() => {
+    initDevConsole();
+  }, []);
+
+  return null;
+}
