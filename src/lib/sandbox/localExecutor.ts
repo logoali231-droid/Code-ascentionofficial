86da7be5f0e81cfe0773fe5d776842a6ccadf4c3
Logoali231 <logoali231@gmail.com>
2026-05-20 09:26:17 -0300

    Update brain.ts 2

--- src/lib/sandbox/localExecutor.ts
+++ src/lib/sandbox/localExecutor.ts
@@ -1,4 +1,4 @@
-import { IEngineExecutor } from "./types";
+import { IEngineExecutor } from "./engines";
 export class LocalExecutor implements IEngineExecutor {
   async execute(code: string, language: string, signal?: AbortSignal) {
     return runLocal(code, language, signal);
