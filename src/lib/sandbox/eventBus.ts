f60cf23a6a6986fca8577bf121c9951a736c5384
Logoali231 <logoali231@gmail.com>
2026-05-20 19:42:34 -0300

    Create eventBus.ts

---
+++ src/lib/sandbox/eventBus.ts
@@ -0,0 +1,17 @@
+// lib/sandbox/eventBus.ts
+type Subscriber = (event: SandboxEvent) => void;
+
+class SandboxEventBus {
+  private subscribers: Subscriber[] = [];
+
+  publish(event: SandboxEvent) {
+    this.subscribers.forEach(sub => sub(event));
+  }
+
+  subscribe(callback: Subscriber) {
+    this.subscribers.push(callback);
+    return () => { /* remove logic */ };
+  }
+}
+
+export const sandboxBus = new SandboxEventBus();
