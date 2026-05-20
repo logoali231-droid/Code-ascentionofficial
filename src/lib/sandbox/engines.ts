eb71d62d4417b99b2bb2851554e6268e418a16b3
Logoali231 <logoali231@gmail.com>
2026-05-20 09:03:34 -0300

    Update engines.ts

--- src/lib/sandbox/engines.ts
+++ src/lib/sandbox/engines.ts
@@ -1,12 +1,7 @@
-import { IEngineExecutor } from "./types";
-import { LocalExecutor } from "./localExecutor";
-import { WasmExecutor } from "./wasmExecutor";
-import { RemoteExecutor } from "./remoteExecutor";
-import { NeuralExecutor } from "./neuralExecutor";
-
-const EXECUTOR_REGISTRY: Record<string, IEngineExecutor> = {
-  local: new LocalExecutor(),
-  wasm: new WasmExecutor(),
-  remote: new RemoteExecutor(),
-  neural: new NeuralExecutor(),
-};
+export interface IEngineExecutor {
+  execute(
+    code: string,
+    language: string,
+    signal?: AbortSignal
+  ): Promise<SandboxResult>;
+}
