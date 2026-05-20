519355fd426f32eb236fd67517458d3897affe8c
Logoali231 <logoali231@gmail.com>
2026-05-20 09:24:09 -0300

    Update localExecutor.ts

--- src/lib/sandbox/localExecutor.ts
+++ src/lib/sandbox/localExecutor.ts
@@ -1,3 +1,4 @@
+import { IEngineExecutor } from "./types";
 export class LocalExecutor implements IEngineExecutor {
   async execute(code: string, language: string, signal?: AbortSignal) {
     return runLocal(code, language, signal);
